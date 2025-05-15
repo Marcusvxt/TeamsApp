5dd7dac5f50a2bb5509e87441bae24bef80ef301
Marcus <marcusvinicius1501@gmail.com>
2025-05-15 11:49:07 -0300

    Add Files

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { ThemeProvider} from 'styled-components/native';
+import { useFonts , Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
+
+import theme from './src/theme';
+
+import { Routes } from './src/routes'
+
+import * as SystemUI from 'expo-system-ui';
+
+SystemUI.setBackgroundColorAsync('transparent');
+
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular,Roboto_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        { fontsLoaded ? <Routes /> : <Loading />}
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
